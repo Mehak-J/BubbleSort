cf113198df59ffec4093444ce2ebcf59a1dfaa8a
Mehak Jay <mehak.jay@student.allenisd.org>
2022-10-10 10:29:43 -0500

    First Commit

---
+++ main.swift
@@ -0,0 +1,39 @@
+
+//let unsortedWords = ["dino", "cowboy", "baby", "ant"]
+
+var words = [String]()
+
+while let line = readLine() {
+    
+    if (line.count == 0) {break;}
+    words.append(line)
+}
+
+func swap(strings:inout [String], firstIndex:Int, secondIndex:Int) {
+    
+    let tmp = strings[firstIndex]
+    strings[firstIndex] = strings[secondIndex]
+    strings[secondIndex] = tmp
+   
+ }     
+func bubbleSort() {
+    
+    for i in 0 ..< words.count{
+        for j in 1 ..< words.count - i{
+            if(words[j].lowercased() < words[j-1].lowercased()) {
+                swap(strings:&words, firstIndex: j, secondIndex: j-1)
+            }
+        }
+        print(words)
+    }
+}
+
+
+bubbleSort()                   
+
+
+
+
+
+
+///print("pass: \(passCount), Swaps: \(swapCount
